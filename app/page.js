713b842633d8d5d2e4bef1344f8db83bf2f891ca
tahersselim <taher.selim.dev@gmail.com>
2024-08-16 08:54:50 +0300

    init

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import Intro from "./components/Intro/Intro";
 import Footer from "./components/footer/Footer";
-import Skills from "./components/skills/Skills";
-import Works from "./components/work/Works";
+import Skills from "./components/skills/skills";
+import Works from "./components/work/works";
 import Contact from "./components/contact/contact";
 import Navbar from "./components/Nav bar/navbar";
 export default function Home() {
